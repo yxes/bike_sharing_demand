be0c3d8074fc956e9cf15ed01716efb863a24566
Steve Wells <yxes123@gmail.com>
2015-04-09 19:09:18 -0700

    Exploratory Analysis
    * Looked at coefficients in python
    * Created RMarkdown File of Exploration
    * Created Custom Prediction Algorithm in Perl
    * Provided Final Predictions in:
    
    explore/perl_prediction.csv

---
+++ explore/coefficients.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# We are using Python 3 here
+import zipfile
+import numpy as np
+import pandas
+import matplotlib.pyplot as plt
+import scipy
+import scipy.stats
+import io
+
+ZIPFILE = '../original_data/train.csv.zip'
+
+def rental_histogram(bikedf):
+    '''
+    Examine the rental counts in our bike data and determine what
+    distribution the data follows.  
+    
+    Plot two histograms on the same axes to show rentals when the
+    weather is clear vs when it's inclement.
+    '''
+
+    plt.figure()
+    bikedf[bikedf['weather'] == 1]['count'].hist(bins = 50, label="Clear", facecolor="yellow")
+    bikedf[bikedf['weather'] > 1]['count'].hist(bins = 50, label="Inclement", facecolor="blue")
+    plt.legend(title="Weather")
+    plt.title(r'Bike Rentals Histogram')
+    plt.xlabel("Number of Bike Rentals")
+    plt.ylabel("Freqency")
+
+    return plt
+
+def rental_histogram_inclement(bikedf):
+    '''
+    Examine the rental counts in our bike data limiting it to the
+    two types of inclement weather.  If these turn out to be
+    related then we can use them as a single entity.
+    '''
+
+    plt.figure
+    bikedf[bikedf['weather'] == 2]['count'].hist(bins = 50, label="Mist", facecolor="grey")
+    bikedf[bikedf['weather'] == 3]['count'].hist(bins = 50, label="Rain", facecolor="blue")
+    plt.legend(title="Inclement Weather")
+    plt.title(r'Bike Rentals (Inclement) Histogram')
+    plt.xlabel("Number of Bike Rentals")
+    plt.ylabel("Frequency")
+
+    return plt
+
+def mann_whitney_plus_means(v1, v2):
+    '''
+    Takes the means and runs a Mann Witney U-test on the 'count' column.
+
+    Returns:
+      * mean of entries when weather is clear
+      * mean of entries when for inclement weather
+      * Mann-Whitney U-Statistic and p-value comparing the number of rentals
+      when it's clear vs when it's not
+    '''
+    (U, p) = scipy.stats.mannwhitneyu(v1, v2)
+
+    return np.mean(v1), np.mean(v2), U, p * 2
+
+def explore(zip_file):
+    csv_file = zip_file[:-4] # strip off the .zip
+    csv_file = csv_file.split('/')[-1] # strip off any directories so we just have the file
+
+    fh = open(zip_file, 'rb')
+    zf = zipfile.ZipFile(fh)
+    data = io.BytesIO(zf.read(csv_file))
+    df = pandas.read_csv(data)
+
+    print('''
+    QUESTION
+    ========
+    Can we combine any of our weather types into a single coefficient for our final
+    algorithm?
+    ''')
+    input("\nPress Enter to continue...")
+
+    print("\nThe histogram for the two sets are both right-skewed so we'll be using\n"
+          "a Mann-Whitney U test as opposed to a t-test for our results")
+
+    plt = rental_histogram(df)
+    plt.show()
+
+    clear_mean, inclement_mean, U, p = mann_whitney_plus_means(df[df['weather'] == 1]['count'],
+                                                               df[df['weather'] > 1]['count'])
+    print("-"*28)
+    print("\n Clear vs Inclement Weather")
+    print("-"*28)
+    print("\nMann Whitney Results:\n\tClear Mean:", clear_mean,"\n\tInclement Mean:", inclement_mean,
+            "\n\tMann Whitney U:", U, "\n\tp-value:", p,"\n")
+
+    # Conclusions
+    if (p < 0.05):
+        print("Since the p-value is less than 0.05 we can conclude that there is a\n"
+              "difference between the distributions.  It is statistically significant\n"
+              "enough that we can conclude that when the weather is clear it affects\n"
+              "the number of riders.")
+        if (clear_mean > inclement_mean):
+            print("\nFurther to that since the mean number of riders on a clear day\n"
+                  "is greater than the inclement days, we can conclude that more\n"
+                  "riders go out when the days are clear. (which may be obvious)")
+        elif (clear_mean < inclement_mean):
+            print("\nStrangely more riders go out when the weather is bad.\n")
+        else:
+            print("\n...and yet the means are the same no matter what the weather...\n")
+    else:
+        print("Since the p-value is greater than 0.05 we can conclude that there is no\n"
+              "statiscal difference between the number of riders no matter what the\n"
+              "weather is\n")
+
+    print("\nThere is only one entry for severe weather so the next step is to see if\n"
+          "the last two types of inclement weather could have been derived from the same\n"
+          "population\n")
+
+    plt = rental_histogram_inclement(df)
+    plt.show()
+
+    mist_mean, rain_mean, U, p = mann_whitney_plus_means(df[df['weather'] == 2]['count'],
+                                                               df[df['weather'] == 3]['count'])
+
+    print("-"*26)
+    print("  Misty vs Rainy Weather")
+    print("-"*26)
+    print("\nMann Whitney Results:\n\tMist Mean:", mist_mean,"\n\tRain Mean:", rain_mean,
+            "\n\tMann Whitney U:", U, "\n\tp-value:", p,"\n")
+
+    print('''
+    ==========
+    CONCLUSION
+    ==========
+    With such a small p-value we have to conclude that we need to treat each of our 
+    weather types as separate coefficients in our final algorithm.
+    ''')
+
+if __name__ == "__main__":
+    explore(ZIPFILE)
---
+++ explore/prediction.pl
@@ -0,0 +1,165 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+###
+# Fills in missing values from https://www.kaggle.com/c/bike-sharing-demand
+#
+# Author: Stephen D. Wells
+# Date: Apr, 4th 2015
+# Ver: 1.0
+#
+# Usage: $0 > prediction.csv
+#
+# Loads all the test data and tosses away everything
+# but the day (YYYYmmdd), hour, holiday and workingday values
+#
+# Then runs through the training data in a single pass and 
+# builds out the results according the following rules:
+#
+# 1. reads in a line of training data and strips out everything
+# except the day (YYYYmmdd), hour, holiday, workingday and count.
+#
+# 2. compares it's day to the current test day we are working
+# on. If it's later than today we process it, otherwise we
+# record it according to it's holiday and workingday values
+# and move on to the next line.
+#
+# 3. If on the other hand, it's later than the current test day
+# and it matches our holiday and working day we calculate the
+# count values by using the following formula.
+#
+# Pv = Past Value (riders for that same hour)
+# Fv = Future Value
+# Pd = Past Days (number of days from our test day)
+# Fd = Future Days (days into the future from our test day)
+#
+# Total_Change = Fv - Pv
+# Total_Distance = Fd - Pd
+# Change_per_Day = Total_Change / Total_Distance
+#
+# COUNT = (Change_per_Day * Pd) + Pv
+#  ... or ...
+# COUNT = Fd - (Change_per_Day * Fd)
+#
+# We are just taking the intervals and trying to get as
+# close to it linearly between the closest sets of data that
+# we have.
+
+# Fill in the location of your training and test dataset
+# expects the data to be named 'train.csv' and 'test.csv' zipped
+#
+use constant DATA_DIR => '../original_data/';
+
+# Ensure these two libraries are installed
+use IO::Uncompress::Unzip qw(unzip $UnzipError);
+use Date::Calc qw(Delta_Days);
+
+#####  START ######
+#
+my ($train, $test) = fetch_data();
+
+# gather up our test data
+my @td;
+{
+  my $c = 0;
+  for (split /[\r\n\cM]+/, $test) {
+      next if (!$c && ++$c); # skip the header row
+
+      my @line = split /,/, $_;
+
+      # test data: day / hour / holiday / workingday
+      push(@td, [format_day_hour($line[0]), $line[2], $line[3]]); 
+  }
+}
+
+# print out our header
+print '"datetime","count"',"\n";
+
+# Mesh the two sets together
+{
+  my $test_day = shift(@td);
+  my @past_value; # contains [holiday][workingday][hour] = [YYYYmmdd, count]
+  my $c = 0; # to skip the header
+  for (split /[\r\cM\n]+/, $train) {
+      next if (!$c && ++$c); # skip the header row
+
+      # input line
+      my @line = split /,/, $_;
+
+      # output: day, hour, holiday, workingday, count
+      my($dy, $hr, $hol, $wrk, $count) = (
+	    format_day_hour($line[0]),
+	    $line[2],   # holiday
+	    $line[3],   # workingday
+	    $line[-1]); # count
+
+      my($tdy, $thr, $thol, $twrk) = @$test_day;
+      if ($test_day && $dy > $test_day->[0] && $hr == $thr && $wrk == $twrk) {
+         my $past_count = $past_value[$thol][$twrk][$thr]->[1];
+         my $past_distance = calculate_distance($past_value[$thol][$twrk][$thr]->[0], $test_day->[0]);
+         my $future_distance = calculate_distance($test_day->[0], $dy);
+
+	 # round the count using int(val + 0.5)
+         my $final_count = int(calculate_count($past_count, $count, $past_distance, $future_distance) + 0.5);
+
+         print join('-', unpack('A4A2A2', $test_day->[0])), ' ', $thr, ':00:00,', $final_count, "\n";
+
+         $test_day = shift(@td)
+      }
+
+      $past_value[$hol][$wrk][$hr] = [$dy, $count];
+  }
+}
+
+##### SUBROUTINES #####
+#
+
+# Inputs: 2 X YYYYmmdd as integers
+# Output: days between the integers
+sub calculate_distance {
+    my($ptA, $ptB) = @_;
+    ($ptA, $ptB) = ($ptB, $ptA) if ($ptA > $ptB);
+
+return Delta_Days(map unpack('A4A2A2', $_), ($ptA, $ptB)) 
+}
+
+# past = the count value of the past
+# future = the count value of the future
+# past_distance = the distance from the present to the past
+# future_distance = the distance from the present to the future
+sub calculate_count { # where 'count' is the number of riders
+    my($past, $future, $past_distance, $future_distance) = @_;
+
+    my $total_change = $future - $past;
+    my $total_distance = $future_distance + $past_distance;
+
+    my $change_per_increment = $total_change / $total_distance;
+
+return ($change_per_increment * $past_distance) + $past;
+}
+
+# Input: YYYY-mm-dd HH:00:00
+# Output: ('YYYYmmdd', 'HH')
+sub format_day_hour {
+    my $datetime = shift;
+
+ join('',
+      map substr($datetime, $$_[0], $$_[1]),
+	    	([0,4],[5,2],[8,2])), 		# YYYY-mm-dd
+      substr($datetime, 11, 2)	      		# HH
+}
+
+# just loads the whole thing into RAM
+sub fetch_data {
+    my($train, $test);
+
+    unzip DATA_DIR. 'train.csv.zip' => \$train, Name => 'train.csv'
+	or die "unzip failed: $UnzipError";
+    
+    unzip DATA_DIR. 'test.csv.zip' => \$test, Name => 'test.csv'
+	or die "unzip failed: $UnzipError";
+
+
+return($train, $test)
+}
